b10e5e9d5d6459d7f4a89fe929400b4f09b7d531
Home <Home@Home-Laptop.Home>
2020-10-20 19:53:08 +0100

    Commit End of Day 20/10/2020

--- src/test/java/PagesTheInternetHerokuApp/HomePage.java
+++ src/test/java/PagesTheInternetHerokuApp/HomePage.java
@@ -1,5 +1,18 @@
 package PagesTheInternetHerokuApp;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
 
+import Utilties.CommonMethod;
+
+public class HomePage extends CommonMethod {
+
+@Test (priority = 0, enabled =  true)
+public void clickAbTestingLink(){
+	
+	driver.findElement(By.xpath("/html/body/div[2]/div/ul/li[1]/a")).click();
+	
+	
+}	
+	
 }
---
+++ src/test/java/Utilties/CommonMethod.java
@@ -0,0 +1,36 @@
+package Utilties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class CommonMethod {
+	public WebDriver driver;
+@BeforeMethod (groups = {"Smoke","Regression"})
+@Parameters ("Browser") // The name should match with testng parameter name
+
+// initiateBrowser method revokes browser using Browser parameter
+	public void initiateBrowser(String browser) throws InterruptedException {
+if (browser.equals("Chrome"))
+{
+		System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+else{
+	System.setProperty("webdriver.gecko.driver", ".//geckodriver.exe");
+	driver = new FirefoxDriver();
+}
+
+driver.get("http://the-internet.herokuapp.com/");
+driver.manage().window().maximize(); 
+	}
+
+@AfterMethod (groups = {"Smoke","Regression"})
+	public void tearDown() {
+		driver.quit();
+	}
+}
+
